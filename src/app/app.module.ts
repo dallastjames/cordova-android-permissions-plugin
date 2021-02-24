df48e4ff7e0c401155b019a076e5df2dbfc0ff20
Dallas James <dallastjames@gmail.com>
2021-02-24 12:42:06 -0700

    init to win it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AndroidPermissions } from '@ionic-enterprise/android-permissions/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    AndroidPermissions,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AndroidPermissions } from '@ionic-enterprise/android-permissions/ngx';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(private _p: AndroidPermissions) {}
 
-  constructor() {}
+  public async cameraPermissions(): Promise<void> {
+    console.log(
+      'camera permission',
+      await this._p.checkPermission(this._p.PERMISSION.CAMERA)
+    );
+    console.log(
+      'request permission',
+      await this._p.requestPermission(this._p.PERMISSION.CAMERA)
+    );
+  }
 
+  public async bleAdminPermissions(): Promise<void> {
+    console.log(
+      'ble admin permission',
+      await this._p.checkPermission(this._p.PERMISSION.BLUETOOTH_ADMIN)
+    );
+    console.log(
+      'request permission',
+      await this._p.requestPermission(this._p.PERMISSION.BLUETOOTH_ADMIN)
+    );
+  }
 }
